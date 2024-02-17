f60d644c491ac6f82b5183b416a0396786c6d18a
Aiyuki Kawano <kawanoaiyuki@gmail.com>
2024-02-18 06:20:55 +1000

    update

--- app.py
+++ app.py
@@ -2,53 +2,53 @@ from flask import Flask, request, send_file, jsonify
 import pyautogui
 import time
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app) # Enable CORS for all routes
 
 stored_url = ""  # Variable to store the URL temporarily
 
-@app.route('/capture-screenshot', methods=['GET', 'POST'])
 
-# Define and store the current time
 
-def capture_screenshot():
-    if request.method == 'POST':
-        customer_name = request.json['customerName']
-        # customer_name = request.form.get('customerName') # Get the customer name from the form input
-        current_time = time.strftime("%Y%m%d%H%M%S")
-        print(customer_name)
-        # Define the coordinates and size of the region to capture
-        left = 695  # X-coordinate of the top-left corner of the region
-        top = 260   # Y-coordinate of the top-left corner of the region
-        width = 525  # Width of the region
-        height = 525  # Height of the region
-        
-        # Capture the screenshot of the specified region
-        screenshot = pyautogui.screenshot(region=(left, top, width, height))
-        file_name = f"{current_time}-2{customer_name}.png"
-        print(file_name)
-        screenshot.save(file_name)
-        
-        # Return the screenshot file as an attachment
-        return send_file(file_name, as_attachment=True)
-    else:
-        return 'Method Not Allowed', 405
+@app.route('/save-images', methods=['POST'])
+def save_images():
+    data = request.json
+    customer_name = data.get('customerName')
+    
+    # Create a folder with the customer's name if it doesn't exist
+    folder_path = os.path.join(os.getcwd(), customer_name)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    
+    # Save the sample image
+    sample_image_data = request.files['sampleImage']
+    sample_image_path = os.path.join(folder_path, 'sample_image.png')
+    sample_image_data.save(sample_image_path)
+    
+    # Save the screenshot image
+    screenshot_image_data = request.files['screenshotImage']
+    screenshot_image_path = os.path.join(folder_path, 'screenshot_image.png')
+    screenshot_image_data.save(screenshot_image_path)
+    
+    return jsonify(message="Images saved successfully")
     
 @app.route('/store-url', methods=['POST'])
 def store_url():
     global stored_url
     data = request.json
     stored_url = data.get('url')
+    print(stored_url)
     return jsonify(message="URL stored successfully")
 
 @app.route('/get-url', methods=['GET'])
 def get_url():
     return jsonify(url=stored_url)
+    print(url)
 
 @app.route('/')
 def hello():
        return 'Hello, World!'
    
 if __name__ == '__main__':
-       app.run()
+       app.run("localhost", 5000)
